78fc3297911de5eff7ff086e1b9492d479af8e61
Mannat <mannat@kapturecrm.com>
2025-11-28 18:56:12 +0530

    test

--- index.js
+++ index.js
@@ -68,5 +68,5 @@ app.post("/issues", async (req, res) => {
 });
   
 app.listen(process.env.PORT, () => {
-  console.log("API running on port " + process.env.PORT);
+  console.log("API runningasdasd on port " + process.env.PORT);
 });
